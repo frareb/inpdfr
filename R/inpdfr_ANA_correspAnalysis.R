5b408ccd90bc89f8bb94feb970e40d3d4bdaa8c1
frareb <francois.rebaudo@ird.fr>
2016-03-18 11:11:55 -0400

    R.devices pkg for output files

--- R/inpdfr_ANA_correspAnalysis.R
+++ R/inpdfr_ANA_correspAnalysis.R
@@ -8,6 +8,7 @@
 #'   directory.
 #' @param mwidth The width of the plot in pixels.
 #' @param mheight The height of the plot in pixels.
+#' @param formatType The format for the output file ("eps", "pdf", "png", "svg", "tiff", "jpeg", "bmp").
 #' @param ... Additional arguments from the \code{\link[ca]{ca}} function.
 #' @return The results of the \code{\link[ca]{ca}} function.
 #' @examples
@@ -15,19 +16,30 @@
 #' doCA(wordF=myDF)
 #' }
 #' @export
-doCA<-function(wordF,getPlot=TRUE,mwidth=800,mheight=800,...){
-  if(ncol(wordF)>3){
-    freqWord<-0
-    mergedDClean<-wordF[,2:length(wordF[1,])][colSums(wordF[,2:length(wordF[1,])])>freqWord]
-    mergedDClean<-as.data.frame(cbind(as.character(wordF[,1]),mergedDClean))
-    mergedDClean[,1]<-as.character(mergedDClean[,1])
-    mergedDClean<-mergedDClean[which(rowSums(mergedDClean[,2:length(mergedDClean[1,])])>freqWord),]
-    fitCa <- ca::ca(obj=as.matrix(mergedDClean[,2:length(mergedDClean[1,])],colnames=mergedDClean[,1]),nd=NA,...)
-
-    if(getPlot==TRUE){
-      grDevices::png(filename="RESULTS/CorAnalysis.png",width = mwidth, height = mheight)
+doCA <- function(wordF, getPlot = TRUE, mwidth = 800, mheight = 800, formatType = "png", ...){
+  ## create RESULTS folder
+  subDir <- "RESULTS"
+  dir.create(file.path(getwd(), subDir), showWarnings = FALSE)
+  ## make ca
+  if(ncol(wordF) > 3){
+    freqWord <- 0
+    mergedDClean <- wordF[,2:length(wordF[1,])][colSums(wordF[,2:length(wordF[1,])]) > freqWord]
+    mergedDClean <- as.data.frame(cbind(as.character(wordF[,1]), mergedDClean))
+    mergedDClean[,1] <- as.character(mergedDClean[,1])
+    mergedDClean <- mergedDClean[which(rowSums(mergedDClean[,2:length(mergedDClean[1,])]) > freqWord),]
+    fitCa <- ca::ca(obj = as.matrix(mergedDClean[,2:length(mergedDClean[1,])], 
+      colnames = mergedDClean[,1]), nd = NA, ...)
+    if(getPlot == TRUE){
+      R.devices::devEval(type = formatType, name = "CorAnalysis",
+       aspectRatio = mheight / mwidth,
+       scale = do.call(function(){if((mheight / mwidth) <= 1) {
+         x <- max(mheight / 480, mwidth / 480)} else {
+           x <- min(mheight / 480, mwidth / 480)}
+         return(x)}, list())
+       , path = file.path(getwd(), subDir), {
         try(graphics::plot(fitCa,labels=c(0,2)),silent=TRUE)
-      grDevices::dev.off()
+       }
+      )
     }
     return(fitCa)
   }

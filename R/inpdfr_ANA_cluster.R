a54c2557a872af874ff2462dc73e0c856d7ae938
frareb <francois.rebaudo@ird.fr>
2016-03-17 20:00:31 -0400

    R.devices pkg for output files

--- R/inpdfr_ANA_cluster.R
+++ R/inpdfr_ANA_cluster.R
@@ -11,6 +11,7 @@
 #' @param getPlot If \code{TRUE}, save the cluster plot in the RESULTS directory.
 #' @param mwidth The width of the plot in pixels.
 #' @param mheight The height of the plot in pixels.
+#' @param formatType The format for the output file ("eps", "pdf", "png", "svg", "tiff", "jpeg", "bmp").
 #' @param ... Additional arguments from the \code{\link[stats]{hclust}} function.
 #' @return An object of class \code{\link[stats]{hclust}}.
 #' @examples
@@ -18,24 +19,43 @@
 #' doCluster(wordF=myDF,myMethod="ward.D2")
 #' }
 #' @export
-doCluster<-function(wordF,myMethod="ward.D2",gp=FALSE,nbGp=5,getPlot=TRUE,mwidth=800,mheight=800,...){
-  if(ncol(wordF)>3){
-    fitClust <-  stats::hclust(stats::dist(t(as.matrix(wordF[,2:length(wordF[1,])]))),method=myMethod,...) # ,colnames=fileNames
+doCluster <- function(wordF, myMethod = "ward.D2", gp = FALSE, nbGp = 5, getPlot = TRUE, mwidth = 800,
+  mheight = 800, formatType = "png", ...){
+  ## create RESULTS folder
+  subDir <- "RESULTS"
+  dir.create(file.path(getwd(), subDir), showWarnings = FALSE)
+  ## make cluster analysis
+  if(ncol(wordF) > 3){
+    fitClust <-  stats::hclust(stats::dist(t(as.matrix(wordF[,2:length(wordF[1,])]))),
+      method = myMethod, ...)
 
-    if(getPlot==TRUE){
-      if (gp==TRUE){
-        grDevices::png(filename="RESULTS/HCLUST.png",width = mwidth, height = mheight)
-          try(graphics::plot(fitClust,hang=-1),silent=TRUE)
-          groups <- try(stats::cutree(fitClust, k=nbGp),silent=TRUE)
-          try(stats::rect.hclust(fitClust, k=nbGp, border="red"),silent=TRUE)
-        grDevices::dev.off()
-
+    if(getPlot == TRUE){
+      if (gp == TRUE){
+        ## strat graph
+        R.devices::devEval(type = formatType, name = "HCLUST",
+          aspectRatio = mheight / mwidth,
+          scale = do.call(function(){if((mheight / mwidth) <= 1) {
+            x <- max(mheight / 480, mwidth / 480)} else {
+              x <- min(mheight / 480, mwidth / 480)}
+            return(x)}, list())
+          , path = file.path(getwd(), subDir), {
+            try(graphics::plot(fitClust, hang = -1), silent = TRUE)
+            groups <- try(stats::cutree(fitClust, k = nbGp), silent = TRUE)
+            try(stats::rect.hclust(fitClust, k = nbGp, border = "red"), silent = TRUE)
+          }
+        )
+        # end graph
       }else{
-
-        grDevices::png(filename="RESULTS/HCLUST.png",width = mwidth, height = mheight)
-          try(graphics::plot(fitClust),silent=TRUE)
-        grDevices::dev.off()
-
+        R.devices::devEval(type = formatType, name = "HCLUST",
+          aspectRatio = mheight / mwidth,
+          scale = do.call(function(){if((mheight / mwidth) <= 1) {
+            x <- max(mheight / 480, mwidth / 480)} else {
+              x <- min(mheight / 480, mwidth / 480)}
+            return(x)}, list())
+          , path = file.path(getwd(), subDir), {
+          try(graphics::plot(fitClust), silent = TRUE)
+          }
+        )
       }
     }
     return(fitClust)
@@ -54,6 +74,7 @@ doCluster<-function(wordF,myMethod="ward.D2",gp=FALSE,nbGp=5,getPlot=TRUE,mwidth
 #' @param getPlot If \code{TRUE}, save the k-means cluster plot in the RESULTS directory.
 #' @param mwidth The width of the plot in pixels.
 #' @param mheight The height of the plot in pixels.
+#' @param formatType The format for the output file ("eps", "pdf", "png", "svg", "tiff", "jpeg", "bmp").
 #' @param ... Additional arguments from the \code{\link[stats]{kmeans}} function.
 #' @return An object of class kmeans (see \code{\link[stats]{kmeans}}).
 #' @examples
@@ -61,17 +82,26 @@ doCluster<-function(wordF,myMethod="ward.D2",gp=FALSE,nbGp=5,getPlot=TRUE,mwidth
 #' doKmeansClust(wordF=myDF)
 #' }
 #' @export
-doKmeansClust<-function(wordF,nbClust=4,nbIter=10,algo="Hartigan-Wong",getPlot=TRUE,mwidth=800,mheight=800,...){
+doKmeansClust <- function(wordF, nbClust = 4, nbIter = 10, algo = "Hartigan-Wong", getPlot = TRUE,
+  mwidth = 800, mheight = 800, formatType = "png", ...){
+  ## create RESULTS folder
+  subDir <- "RESULTS"
+  ## make kmeans-cluster analysis
   if(ncol(wordF)>3){
     dd <-(stats::dist(t(as.matrix(wordF[,2:length(wordF[1,])])), method="euclidian"))# ,colnames=fileNames
     kfit <- stats::kmeans(x=dd, centers=nbClust, iter.max=nbIter, algorithm=algo,...)
-
     if(getPlot==TRUE){
-      grDevices::png(filename="RESULTS/KMEANCLUST.png",width = mwidth, height = mheight)
+      R.devices::devEval(type = formatType, name = "KMEANCLUST",
+        aspectRatio = mheight / mwidth,
+        scale = do.call(function(){if((mheight / mwidth) <= 1) {
+          x <- max(mheight / 480, mwidth / 480)} else {
+            x <- min(mheight / 480, mwidth / 480)}
+          return(x)}, list())
+        , path = file.path(getwd(), subDir), {
         try(cluster::clusplot(as.matrix(dd), kfit$cluster, color=T, shade=T, labels=2, lines=0),silent=TRUE)
-      grDevices::dev.off()
+        }
+      )
     }
-
     return(kfit)
   }
 }

2742f1051d73b777c0b197ba313edb0f3ac45816
frareb <francois.rebaudo@ird.fr>
2016-03-18 11:20:17 -0400

    R.devices pkg for output files

--- R/inpdfr_ANA_ECO_entropart.R
+++ R/inpdfr_ANA_ECO_entropart.R
@@ -18,6 +18,7 @@
 #'    the \code{DivProfile} analisis is saved in the RESULTS directory.
 #' @param mwidth The width of the plot in pixels.
 #' @param mheight The height of the plot in pixels.
+#' @param formatType The format for the output file ("eps", "pdf", "png", "svg", "tiff", "jpeg", "bmp").
 #' @return A \code{MetaCommunity} object (see \code{\link[entropart]{entropart-package}}).
 #' @examples
 #' \dontrun{
@@ -27,78 +28,107 @@
 #' @importFrom entropart DivPart
 #' @importFrom entropart DivEst
 #' @importFrom entropart DivProfile
-doMetacomEntropart<-function(wordF,getPlot=c(TRUE,TRUE,TRUE,TRUE),getTextSink=c(TRUE,TRUE,TRUE,TRUE),mwidth=800,mheight=800){
-
+doMetacomEntropart <- function(wordF, getPlot = c(TRUE, TRUE, TRUE, TRUE), 
+  getTextSink = c(TRUE, TRUE, TRUE, TRUE), mwidth = 800, mheight = 800, formatType = "png"){
+  ## create RESULTS folder
   subDir <- "RESULTS"
   dir.create(file.path(getwd(), subDir), showWarnings = FALSE)
+  ## make entropart analysis
+  metacomDB <- wordF
+  names(metacomDB)[1] <- "Species"
+  metaCom <- entropart::MetaCommunity(Abundances = metacomDB[,2:length(wordF[1,])])
 
-  metacomDB<-wordF
-  names(metacomDB)[1]<-"Species"
-  metaCom<-entropart::MetaCommunity(Abundances=metacomDB[,2:length(wordF[1,])])
-
-  if(getPlot[1]==TRUE){
-    grDevices::png(filename="RESULTS/entropart_MetaCom.png",width = mwidth, height = mheight)
-      try(graphics::plot(metaCom),silent=TRUE)
-    grDevices::dev.off()
+  if(getPlot[1] == TRUE){
+    R.devices::devEval(type = formatType, name = "entropart_MetaCom",
+      aspectRatio = mheight / mwidth,
+      scale = do.call(function(){if((mheight / mwidth) <= 1) {
+        x <- max(mheight / 480, mwidth / 480)} else {
+          x <- min(mheight / 480, mwidth / 480)}
+        return(x)}, list())
+      , path = file.path(getwd(), subDir), {
+        try(graphics::plot(metaCom), silent = TRUE)
+      }
+    )
   }
 
-  if(getTextSink[1]==TRUE){
+  if(getTextSink[1] == TRUE){
     sink('RESULTS/entropart_MetaCom.txt')
       cat('\n#######################\n### SUMMARY         ###\n#######################\n')
-      try(summary(metaCom),silent=TRUE)
+      try(summary(metaCom), silent = TRUE)
       cat('\n#######################\n### RESULTS         ###\n#######################\n')
-      try(print(metaCom),silent=TRUE)
+      try(print(metaCom), silent = TRUE)
     sink()
   }
 
-  mDP<-DivPart(MC=metaCom) # Diversity partition
+  mDP <- DivPart(MC = metaCom) # Diversity partition
 
-  if(getPlot[2]==TRUE){
-    grDevices::png(filename="RESULTS/entropart_DivPart.png",width = mwidth, height = mheight)
-      try(graphics::plot(mDP),silent=TRUE)
-    grDevices::dev.off()
+  if(getPlot[2] == TRUE){
+    R.devices::devEval(type = formatType, name = "entropart_DivPart",
+     aspectRatio = mheight / mwidth,
+     scale = do.call(function(){if((mheight / mwidth) <= 1) {
+       x <- max(mheight / 480, mwidth / 480)} else {
+         x <- min(mheight / 480, mwidth / 480)}
+       return(x)}, list())
+     , path = file.path(getwd(), subDir), {
+      try(graphics::plot(mDP), silent = TRUE)
+     }
+    )
   }
 
-  if(getTextSink[2]==TRUE){
+  if(getTextSink[2] == TRUE){
     sink('RESULTS/entropart_DivPart.txt')
       cat('\n#######################\n### SUMMARY         ###\n#######################\n')
-      try(summary(mDP),silent=TRUE)
+      try(summary(mDP), silent = TRUE)
       cat('\n#######################\n### RESULTS         ###\n#######################\n')
-      try(print(mDP),silent=TRUE)
+      try(print(mDP), silent = TRUE)
     sink()
   }
 
-  mDE<-DivEst(MC=metaCom) # Diversity estimation
+  mDE <- DivEst(MC = metaCom) # Diversity estimation
 
-  if(getPlot[3]==TRUE){
-    grDevices::png(filename="RESULTS/entropart_DivEst.png",width = mwidth, height = mheight)
-      try(graphics::plot(mDE),silent=TRUE)
-    grDevices::dev.off()
+  if(getPlot[3] == TRUE){
+    R.devices::devEval(type = formatType, name = "entropart_DivEst",
+     aspectRatio = mheight / mwidth,
+     scale = do.call(function(){if((mheight / mwidth) <= 1) {
+       x <- max(mheight / 480, mwidth / 480)} else {
+         x <- min(mheight / 480, mwidth / 480)}
+       return(x)}, list())
+     , path = file.path(getwd(), subDir), {
+      try(graphics::plot(mDE), silent = TRUE)
+     }
+    )
   }
 
-  if(getTextSink[3]==TRUE){
+  if(getTextSink[3] == TRUE){
     sink('RESULTS/entropart_DivEst.txt')
       cat('\n#######################\n### SUMMARY         ###\n#######################\n')
-      try(summary(mDE),silent=TRUE)
+      try(summary(mDE), silent = TRUE)
       cat('\n#######################\n### RESULTS         ###\n#######################\n')
-      try(print(mDE),silent=TRUE)
+      try(print(mDE), silent = TRUE)
     sink()
   }
 
-  mDProf<-DivProfile(MC=metaCom) # Diversity profiles
+  mDProf <- DivProfile(MC = metaCom) # Diversity profiles
 
-  if(getPlot[4]==TRUE){
-    grDevices::png(filename="RESULTS/entropart_DivProf.png",width = mwidth, height = mheight)
-      try(graphics::plot(mDProf),silent=TRUE)
-    grDevices::dev.off()
+  if(getPlot[4] == TRUE){
+    R.devices::devEval(type = formatType, name = "entropart_DivProf",
+     aspectRatio = mheight / mwidth,
+     scale = do.call(function(){if((mheight / mwidth) <= 1) {
+       x <- max(mheight / 480, mwidth / 480)} else {
+         x <- min(mheight / 480, mwidth / 480)}
+       return(x)}, list())
+     , path = file.path(getwd(), subDir), {
+      try(graphics::plot(mDProf), silent = TRUE)
+     }
+    )
   }
 
-  if(getTextSink[4]==TRUE){
+  if(getTextSink[4] == TRUE){
     sink('RESULTS/entropart_DivProf.txt')
       cat('\n#######################\n### SUMMARY         ###\n#######################\n')
-      try(summary(mDProf),silent=TRUE)
+      try(summary(mDProf), silent = TRUE)
       cat('\n#######################\n### RESULTS         ###\n#######################\n')
-      try(print(mDProf),silent=TRUE)
+      try(print(mDProf), silent = TRUE)
     sink()
   }
 
